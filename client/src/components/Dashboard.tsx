8b495624c59721a3adb157f92abf01c203e24246
AngeliniAxel <axel.angelini@gmail.com>
2024-06-08 12:49:52 +0200

    updated dashboard

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,5 +1,4 @@
-import React, { Fragment, useState, useEffect } from 'react';
-
+import React, { Fragment, useState, useEffect, useCallback } from 'react';
 import Button from 'react-bootstrap/Button';
 
 // Define the server API route as a constant
@@ -25,6 +24,7 @@ const Dashboard: React.FC<DashboardProps> = ({ setAuth }) => {
     email: '',
   });
 
+  // Function to update the user state
   const updateUser = (name: string, lastName: string, email: string) => {
     setUser(() => ({
       name: name,
@@ -33,7 +33,8 @@ const Dashboard: React.FC<DashboardProps> = ({ setAuth }) => {
     }));
   };
 
-  async function getUser() {
+  // Function to fetch user data from the server, memoized with useCallback
+  const getUser = useCallback(async () => {
     try {
       const response = await fetch(`${SERVER_API_ROUTE}/dashboard`, {
         method: 'GET',
@@ -42,6 +43,7 @@ const Dashboard: React.FC<DashboardProps> = ({ setAuth }) => {
 
       const parseRes = await response.json();
 
+      // Update the user state with the response data
       updateUser(
         parseRes.user_name,
         parseRes.user_last_name,
@@ -55,12 +57,14 @@ const Dashboard: React.FC<DashboardProps> = ({ setAuth }) => {
         console.error('Unexpected error', err);
       }
     }
-  }
+  }, []); // Empty dependency array ensures this function is created only once
 
+  // useEffect hook to fetch user data when the component mounts
   useEffect(() => {
     getUser();
-  });
+  }, [getUser]); // Include getUser in the dependency array
 
+  // Function to handle user logout
   const logout = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
     localStorage.removeItem('token');
