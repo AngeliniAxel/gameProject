1e2981326adc814a461654acdc2044fb50609dfa
AngeliniAxel <axel.angelini@gmail.com>
2024-06-15 13:58:55 +0200

    Added Redux to handle user info
    Still need to create user logout

--- client/src/components/Dashboard.tsx
+++ client/src/components/Dashboard.tsx
@@ -1,70 +1,25 @@
-import React, { Fragment, useState, useEffect, useCallback } from 'react';
+import React, { Fragment, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
+import { useDispatch, useSelector } from 'react-redux';
 
 import { toast } from 'react-toastify';
-
-// Define the server API route as a constant
-const SERVER_API_ROUTE: string = 'http://localhost:5000';
+import { AppDispatch, RootState } from '../store';
+import { fetchUserData } from '../features/userSlice';
+import { Spinner } from 'react-bootstrap';
 
 // Define the props for the Dashboard component
 interface DashboardProps {
   setAuth: (isAuthenticated: boolean) => void;
 }
 
-// Define the User interface to type the user state
-interface User {
-  name: string;
-  lastName: string;
-  email: string;
-}
-
 const Dashboard: React.FC<DashboardProps> = ({ setAuth }) => {
-  // Initialize the user state with default values
-  const [user, setUser] = useState<User>({
-    name: '',
-    lastName: '',
-    email: '',
-  });
+  const dispatch = useDispatch<AppDispatch>();
+  const userState = useSelector((state: RootState) => state.user);
 
-  // Function to update the user state
-  const updateUser = (name: string, lastName: string, email: string) => {
-    setUser(() => ({
-      name: name,
-      lastName: lastName,
-      email: email,
-    }));
-  };
-
-  // Function to fetch user data from the server, memoized with useCallback
-  const getUser = useCallback(async () => {
-    try {
-      const response = await fetch(`${SERVER_API_ROUTE}/dashboard`, {
-        method: 'GET',
-        headers: { token: localStorage.token },
-      });
-
-      const parseRes = await response.json();
-
-      // Update the user state with the response data
-      updateUser(
-        parseRes.user_name,
-        parseRes.user_last_name,
-        parseRes.user_email
-      );
-    } catch (err) {
-      // Ensure err is of type Error
-      if (err instanceof Error) {
-        console.error(err.message);
-      } else {
-        console.error('Unexpected error', err);
-      }
-    }
-  }, []); // Empty dependency array ensures this function is created only once
-
-  // useEffect hook to fetch user data when the component mounts
+  // Fetch user data when the component mounts
   useEffect(() => {
-    getUser();
-  }, [getUser]); // Include getUser in the dependency array
+    dispatch(fetchUserData());
+  }, [dispatch]);
 
   // Function to handle user logout
   const logout = (e: React.MouseEvent<HTMLButtonElement>) => {
@@ -76,7 +31,11 @@ const Dashboard: React.FC<DashboardProps> = ({ setAuth }) => {
 
   return (
     <Fragment>
-      <h1>Dashboard, welcome {user.name}!!</h1>
+      {userState.loading && <Spinner animation='border' variant='success' />}
+
+      {userState.success && (
+        <h1>Dashboard, welcome {userState.user?.name}!!</h1>
+      )}
 
       <Button variant='primary' type='submit' onClick={logout}>
         Logout
---
+++ client/src/features/userSlice.ts
@@ -0,0 +1,78 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+const SERVER_API_ROUTE: string = 'http://localhost:5000';
+
+interface UserInfo {
+  name: string;
+  lastName: string;
+  email: string;
+}
+
+interface UserInitialState {
+  loading: boolean;
+  user: UserInfo | null;
+  error: string | null;
+  success: boolean;
+}
+
+export const fetchUserData = createAsyncThunk<UserInfo, void>(
+  'user/fetchUserData',
+  async (arg, { rejectWithValue }) => {
+    try {
+      const response = await fetch(`${SERVER_API_ROUTE}/dashboard`, {
+        method: 'GET',
+        headers: { token: localStorage.token },
+      });
+
+      const parseRes = await response.json();
+
+      const userData: UserInfo = {
+        name: parseRes.user_name,
+        lastName: parseRes.user_last_name,
+        email: parseRes.user_email,
+      };
+
+      return userData;
+    } catch (err) {
+      if (err instanceof Error) {
+        return rejectWithValue(err.message);
+      } else {
+        return rejectWithValue('Unexpected error');
+      }
+    }
+  }
+);
+
+const initialState: UserInitialState = {
+  loading: false,
+  user: null, // for user object
+  error: null,
+  success: false, // for monitoring the registration process.
+};
+
+const userSlice = createSlice({
+  name: 'user',
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchUserData.pending, (state) => {
+      state.loading = true;
+      state.error = null;
+    });
+    builder.addCase(
+      fetchUserData.fulfilled,
+      (state, action: PayloadAction<UserInfo>) => {
+        state.loading = false;
+        state.success = true; // registration successful
+        state.user = action.payload;
+      }
+    );
+    builder.addCase(fetchUserData.rejected, (state, action) => {
+      state.loading = false;
+      state.error = action.payload as string;
+    });
+  },
+});
+
+export default userSlice.reducer;
